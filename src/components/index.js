6c79292d047387f28cad087f37b45667f19dfb71
josephlx2014 <46848008+josephlx2014@users.noreply.github.com>
2022-01-23 20:12:06 -0600

    Made excellent improvements

--- src/components/index.js
+++ src/components/index.js
@@ -17,48 +17,105 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: "0",
+      input: [],
       result: 0
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleChange(event) {
-    console.log(event.keyCode);
+  handleClick(event) {
+    switch (event.target.id) {
+      case "clear":
+        this.setState({
+          result: 0,
+          input: []
+        });
+        break;
 
+      case "equals":
+        const expression = this.state.input.join("");
+        const resultValue = eval(expression);
+        this.setState({
+          result: resultValue,
+          input: [resultValue]
+        });
+        break;
+
+      default:
+        if (
+          event.key === "+" ||
+          event.key === "-" ||
+          event.key === "/" ||
+          event.key === "*"
+        ) {
+          this.setState({
+            input: [...this.state.input, event.key],
+            result: this.state.input
+          });
+        } else {
+          this.setState({
+            input: [...this.state.input, event.key],
+            result: event.key
+          });
+        }
+        break;
+    }
+  }
+
+  handleKeyPress(event) {
+    //console.log(event.keyCode);
+    //console.log(event.key);
     switch (event.keyCode) {
       case 13:
+        const expression = this.state.input.join("");
+        const resultValue = eval(expression);
         this.setState({
-          input: event.target.value,
-          result: eval(this.state.input)
+          result: resultValue,
+          input: [resultValue]
+        });
+        break;
+      case 8:
+        this.setState({
+          input: this.state.input.slice(0, this.state.input.length - 1)
+        });
+        break;
+      case 67:
+        this.setState({
+          input: [],
+          result: 0
         });
         break;
       default:
         this.setState({
-          input: event.target.value
+          input: [...this.state.input, event.key]
         });
         break;
     }
   }
 
-  handleClick(event) {}
-
   componentDidMount() {
-    document.addEventListener("keydown", this.handleChange, false);
+    document.addEventListener("keydown", this.handleKeyPress, false);
   }
+
   componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleChange, false);
+    document.removeEventListener("keydown", this.handleKeyPress, false);
   }
 
   render() {
+    function createResultHtml(result) {
+      return {
+        __html: "<h5>" + result + "</h5>"
+      };
+    }
+    function createOperationtHtml(inputArr) {
+      return {
+        __html: "<h5>" + inputArr.join("") + "</h5>"
+      };
+    }
     const buttonList = nums.map((elem, i) => {
       let item = (
-        <button
-          id={elem}
-          className="num-button"
-          onClick={this.handleClick}
-          onKeyPress={this.handleKeyPress}
-        >
+        <button id={elem} className="num-button" onClick={this.handleClick}>
           {i}
         </button>
       );
@@ -67,36 +124,51 @@ class Calculator extends React.Component {
 
     return (
       <div id="display-div">
-        <h6>{this.state.input}</h6>
-        <h6>{this.state.result}</h6>
-        <input
-          type="text"
+        <div
+          id="operation"
+          dangerouslySetInnerHTML={createOperationtHtml(this.state.input)}
+        ></div>
+        <div
           id="display"
-          value={this.state.input}
-          onChange={this.handleChange}
-        ></input>
+          dangerouslySetInnerHTML={createResultHtml(this.state.input)}
+        ></div>
         <hr></hr>
-        <button className="num-button" id="equals" onClick={this.handleClick}>
-          =
-        </button>
-        <button className="num-button" id="add" onClick={this.handleClick}>
-          +
-        </button>
-        <button className="num-button" id="subtract" onClick={this.handleClick}>
-          -
-        </button>
-        <button className="num-button" id="multiply" onClick={this.handleClick}>
-          *
-        </button>
-        <button className="num-button" id="divide" onClick={this.handleClick}>
-          /
-        </button>
-        <button className="num-button" id="decimal" onClick={this.handleClick}>
-          .
-        </button>
-        <button className="num-button" id="clear" onClick={this.handleClick}>
-          C
-        </button>
+        <div onKeyPress={this.handleKeyPress}>
+          <button className="num-button" id="equals" onClick={this.handleClick}>
+            =
+          </button>
+          <button className="num-button" id="add" onClick={this.handleClick}>
+            +
+          </button>
+          <button
+            className="num-button"
+            id="subtract"
+            onClick={this.handleClick}
+          >
+            -
+          </button>
+          <button
+            className="num-button"
+            id="multiply"
+            onClick={this.handleClick}
+          >
+            *
+          </button>
+          <button className="num-button" id="divide" onClick={this.handleClick}>
+            /
+          </button>
+          <button
+            className="num-button"
+            id="decimal"
+            onClick={this.handleClick}
+          >
+            .
+          </button>
+          <button className="num-button" id="clear" onClick={this.handleClick}>
+            C
+          </button>
+        </div>
+
         <hr></hr>
         {buttonList}
       </div>
--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import Counter from "./components/Counter";
-import counter from "./reducers";
 
-const store = createStore(counter);
+import App from "./App";
+
 const rootElement = document.getElementById("root");
-
-const render = () =>
-  ReactDOM.render(
-    <StrictMode>
-      <Counter
-        value={store.getState()}
-        onIncrement={() => store.dispatch({ type: "INCREMENT" })}
-        onDecrement={() => store.dispatch({ type: "DECREMENT" })}
-        onIncrementBy10={() => store.dispatch({ type: "INCREMENTBY10" })}
-      />
-    </StrictMode>,
-    rootElement
-  );
-
-render();
-store.subscribe(render);
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
